9ea41262bae122161c57b8324695c7293c970cdf
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-09-27 16:22:41 +0200

    fix: bonus exercises from 1 to 4

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -6,25 +6,25 @@ const hello = 'hello'; // NON MODIFICARE QUESTA RIGA
 // 1. Cerca un carattere in una stringa
 // Stampa l'INDICE con cui viene trovato
 // il carattere Y nella variabile city
-const cityIndex = 0;
-console.log(city.indexOf("Y")); // Output: 4
+const cityIndex = city.indexOf("Y");
+console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe 
 // Stampa la sottostringa "York" dalla variabile city
-const citySubstring = '';
-console.log(city.substring(4,8)); // Output: "York"
+const citySubstring = city.substring(4,8);
+console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = '';
-console.log(city.replace("York", "Delhi")); // Output:  "New Delhi"
+const cityReplaced = city.replace("York", "Delhi");
+console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
 // Concatena due stringhe usando la sintassi `${var}`
 // e rendi cityNameConcat uguale a 'New York City'
-const cityNameConcat = '';
 let city_name = "City"
-console.log(`${city} ${city_name}`); // Output: "New York City"
+const cityNameConcat = `${city} ${city_name}`;
+console.log(cityNameConcat); // Output: "New York City"
 
 // 5. Tutto in maiscolo
 // Trasforma tutto in maiscuolo la stringa della variabile hello
